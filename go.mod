608e09d348871ef6c358b529126f1bee7102e527
Pavel Tarasov <p.tarasov@alef.im>
2025-02-21 15:32:16 +0300

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module transcribator2000
+
+go 1.23.5
+
+require gopkg.in/tucnak/telebot.v2 v2.5.0
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"gopkg.in/tucnak/telebot.v2"
+)
+
+type TranscriptionResponse struct {
+	Text string `json:"text"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка загрузки .env файла")
+	}
+
+	telegramToken := os.Getenv("TELEGRAM_TOKEN")
+	hfToken := os.Getenv("HF_TOKEN")
+
+	bot, err := telebot.NewBot(telebot.Settings{
+		Token:  telegramToken,
+		Poller: &telebot.LongPoller{Timeout: 10},
+	})
+	if err != nil {
+		fmt.Println("Failed to create bot:", err)
+		return
+	}
+
+	log.Println("🚀 Bot started successfully!")
+
+	bot.Handle("/start", func(m *telebot.Message) {
+		bot.Send(m.Sender, "Hello, I am a transcribator3000")
+	})
+
+	bot.Handle(telebot.OnVoice, func(m *telebot.Message) {
+		fileURL, err := bot.FileURLByID(m.Voice.FileID)
+		if err != nil {
+			bot.Send(m.Sender, "Error getting file link")
+			return
+		}
+
+		filePath, err := downloadFile(fileURL)
+		if err != nil {
+			bot.Send(m.Sender, "Error downloading file")
+			return
+		}
+		defer os.Remove(filePath)
+
+		transcription, err := transcribeAudio(filePath, hfToken)
+		if err != nil {
+			bot.Send(m.Sender, "Error processing transcription")
+			return
+		}
+		bot.Send(m.Sender, transcription, &telebot.SendOptions{ReplyTo: m})
+	})
+
+	bot.Start()
+}
+
+func downloadFile(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	fileType := getFileTypeByUrl(url)
+	randBytes := make([]byte, 16)
+	if _, err := rand.Read(randBytes); err != nil {
+		return "", err
+	}
+	fileName := fmt.Sprintf("./temp/%s.%s", hex.EncodeToString(randBytes), fileType)
+
+	outFile, err := os.Create(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return fileName, nil
+}
+
+func getFileTypeByUrl(url string) string {
+	parts := strings.Split(url, "/")
+	file := parts[len(parts)-1]
+	fileParts := strings.Split(file, ".")
+	if len(fileParts) > 1 {
+		return fileParts[len(fileParts)-1]
+	}
+	return "wav"
+}
+
+func transcribeAudio(filePath string, token string) (string, error) {
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest("POST", "https://api-inference.huggingface.co/models/openai/whisper-large-v3-turbo", bytes.NewReader(fileData))
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "audio/wav")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("failed to get transcription")
+	}
+
+	var result TranscriptionResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return "", err
+	}
+
+	return result.Text, nil
+}
